a572f33cf4e27f403337d1561380d21e43259767
Mshahas786 <mshahas786@gmail.com>
2020-11-30 15:12:12 +1300

    updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
- // Example starter JavaScript for disabling form submissions if there are invalid fields
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
  (function () {
     'use strict';
     window.addEventListener('load', function () {
@@ -16,3 +18,41 @@
       });
     }, false);
   })();
+
+
+  $('.resultsPage').hide();
+
+$(document).ready(function () {
+
+  $('#vechicleSearchBtn').click(function () {
+    $('#searchInputs').hide();
+    $('.resultsPage').show();
+  }); 
+
+
+});
+
+
+
+
+
+
+  function displayCards(j){
+    $('#result').append('<div class="card mb-3" style="max-width: 540px;">'+
+  '<div class="row no-gutters">'+
+    '<div class="col-md-5">'+
+      '<img src="..." class="card-img" alt="..."> </div>'+
+    '<div class="col-md-7">'+
+      '<div class="card-body">'+
+        '<h5 class="card-title">Card title</h5>'+
+        '<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>'+
+        '<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>'+
+      '</div>'+
+  '</div>'+
+  '</div>'+
+'</div>'
+
+);// append ends here
+}; //displayCards
+
+
